6220aa247233a628fb1c0ae044c11aa2de7e0206
Ihor Volokhovych <igorvolokhovych@gmail.com>
2024-10-12 19:22:38 +0300

    feat: bench

--- SpanLinq.Benchmarks/Program.cs
+++ SpanLinq.Benchmarks/Program.cs
@@ -10,9 +10,7 @@ var config = DefaultConfig.Instance
 //    .AddFilter(new AnyCategoriesFilter(new string[] { "Join" }))
     .AddDiagnoser(MemoryDiagnoser.Default)
     .AddValidator(ExecutionValidator.FailOnError)
-    .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.Default, MethodOrderPolicy.Alphabetical))
+    .WithOrderer(new DefaultOrderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Alphabetical))
     .WithOption(ConfigOptions.JoinSummary, true);
 
 BenchmarkRunner.Run(typeof(Program).Assembly, config);
-
-
